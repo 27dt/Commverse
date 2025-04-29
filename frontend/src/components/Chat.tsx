722956602367468e1789ae94210db3041b70992e
gogilbert <jsboersema@hotmail.com>
2025-04-29 12:02:57 -0400

    Finished basic messaging component

--- frontend/src/components/Chat.tsx
+++ frontend/src/components/Chat.tsx
@@ -2,15 +2,26 @@ import { useState } from "react";
 import "../styles/Chat.css";
 
 function Chat() {
-	const [messages, setMessages] = useState([])
+	// State representing current messages as a string array
+	const [messages, setMessages] = useState<string[]>([]);
 
-	function sendMessage(formData: FormData) {
-		console.log(formData.get("message"));
-	}
-	
+	// Arrow function passed into the form upon submit
+	const submitHandler = (formData: FormData) => {
+		// Gets the "message" field from the form which we defined and converts to string
+		const newMessage: string = formData.get("message") as string;
+
+		// Sets the message state to messages (old) with a spread operator and the new message appended
+		setMessages([...messages, newMessage]);
+	};
+
 	return (
 		<div>
-			<form action={sendMessage}>
+			<div>
+				{messages.map((message) => ( // Maps the message state to divs containing that message (arrow function)
+					<div>{message}</div>
+				))}
+			</div>
+			<form action={submitHandler}>
 				<input name="message" />
 				<button type="submit">Send Message</button>
 			</form>
